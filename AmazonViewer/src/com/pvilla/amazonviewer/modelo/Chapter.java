0e4e086ffac4cca5a42aa8994663a3ba5c65c1df
Paola <Paola@Paola-PC>
2018-10-26 11:50:19 -0500

    Se declararon las clases o TDA bases para AmazonViewer

---
+++ AmazonViewer/src/com/pvilla/amazonviewer/modelo/Chapter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pvilla.amazonviewer.modelo;
+
+/**
+ * @Instituto Rumiñahui
+ * @author Paola
+ */
+public class Chapter {
+
+}
